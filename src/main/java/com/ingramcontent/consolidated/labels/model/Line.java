0b5ff734d2235952d771f2f30ed9437da0693cc5
admin <>
2024-09-02 14:02:14 +0000

    {/src/main/java/com/ingramcontent/consolidated/labels/model/Line.java}

--- src/main/java/com/ingramcontent/consolidated/labels/model/Line.java
+++ src/main/java/com/ingramcontent/consolidated/labels/model/Line.java
@@ -6,12 +6,33 @@ package com.ingramcontent.consolidated.labels.model;
 
 public class Line implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Line() {
-    }
+	private int lineSequence;
+	private java.lang.String lineText;
 
+	public Line() {
+	}
 
+	public int getLineSequence() {
+		return this.lineSequence;
+	}
 
+	public void setLineSequence(int lineSequence) {
+		this.lineSequence = lineSequence;
+	}
+
+	public java.lang.String getLineText() {
+		return this.lineText;
+	}
+
+	public void setLineText(java.lang.String lineText) {
+		this.lineText = lineText;
+	}
+
+	public Line(int lineSequence, java.lang.String lineText) {
+		this.lineSequence = lineSequence;
+		this.lineText = lineText;
+	}
 
 }
