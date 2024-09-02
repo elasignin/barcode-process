a5eb15f399f189846bb3a86e5cb4aa6e902b88e4
admin <>
2024-09-02 14:03:42 +0000

    {/src/main/java/com/ingramcontent/consolidated/labels/model/Metadata.java}

--- src/main/java/com/ingramcontent/consolidated/labels/model/Metadata.java
+++ src/main/java/com/ingramcontent/consolidated/labels/model/Metadata.java
@@ -6,12 +6,33 @@ package com.ingramcontent.consolidated.labels.model;
 
 public class Metadata implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Metadata() {
-    }
+	private java.lang.String key;
+	private java.lang.String value;
 
+	public Metadata() {
+	}
 
+	public java.lang.String getKey() {
+		return this.key;
+	}
 
+	public void setKey(java.lang.String key) {
+		this.key = key;
+	}
+
+	public java.lang.String getValue() {
+		return this.value;
+	}
+
+	public void setValue(java.lang.String value) {
+		this.value = value;
+	}
+
+	public Metadata(java.lang.String key, java.lang.String value) {
+		this.key = key;
+		this.value = value;
+	}
 
 }
