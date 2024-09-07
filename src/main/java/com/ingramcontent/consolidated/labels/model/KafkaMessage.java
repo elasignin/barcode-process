bb11e38f043a795450caa4173f1ebb53b473d19c
admin <>
2024-09-07 10:07:54 +0000

    {/src/main/java/com/ingramcontent/consolidated/labels/model/KafkaMessage.java}

--- src/main/java/com/ingramcontent/consolidated/labels/model/KafkaMessage.java
+++ src/main/java/com/ingramcontent/consolidated/labels/model/KafkaMessage.java
@@ -6,12 +6,44 @@ package com.ingramcontent.consolidated.labels.model;
 
 public class KafkaMessage implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public KafkaMessage() {
-    }
+	private java.lang.String topic;
+	private java.lang.String key;
+	private java.lang.String message;
 
+	public KafkaMessage() {
+	}
 
+	public java.lang.String getTopic() {
+		return this.topic;
+	}
 
+	public void setTopic(java.lang.String topic) {
+		this.topic = topic;
+	}
+
+	public java.lang.String getKey() {
+		return this.key;
+	}
+
+	public void setKey(java.lang.String key) {
+		this.key = key;
+	}
+
+	public java.lang.String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(java.lang.String message) {
+		this.message = message;
+	}
+
+	public KafkaMessage(java.lang.String topic, java.lang.String key,
+			java.lang.String message) {
+		this.topic = topic;
+		this.key = key;
+		this.message = message;
+	}
 
 }
